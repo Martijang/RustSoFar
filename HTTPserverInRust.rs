b527d94b002a69dc3d6d00be148aa9c09b8381b7
Martijang <seungwoogo1123@gmail.com>
2025-09-27 12:59:25 +0800

    Update HTTPserverInRust.rs
    Code has been improved.

--- HTTPserverInRust.rs
+++ HTTPserverInRust.rs
@@ -2,19 +2,15 @@ use std::io::{Read, Write};
 use std::net::{TcpListener,TcpStream};
 use std::fs;
 use std::env;
+use std::process::exit;
 use std::thread;
 
-fn responed_as_file(mut stream: TcpStream){
-    let args: Vec<String> = env::args().collect();
-//useage:
-// eg: ./<exe> <your_file_to_send> <Your IP and Port> NOTE: make sure to direct a file properly, like: /User/home/index.html
-//";
-    let mut buffer = [0; 1024];
+fn responed_as_file(mut stream: TcpStream, path: String) ->Result<(), std::io::Error>{
+    let mut buffer = [0u8; 512];
 
-    let _=stream.read(&mut buffer).expect("cannot read a response");
-    println!("{:?}", String::from_utf8_lossy(&buffer[..]));
+    stream.read(&mut buffer)?;
 
-    let file = fs::read_to_string(&args[1]).expect("file not found");
+    let file = fs::read_to_string(path)?;
     let get = b"GET / HTTP/1.1\r\n";
     if buffer.starts_with(get) {
         let response = format!(
@@ -22,41 +18,34 @@ fn responed_as_file(mut stream: TcpStream){
             file.len(),
             file
      ); 
-    //this is where the index.html file will be sent
-    stream.write(
-        response.as_bytes()
-    ).expect("client has lost a connection or error while sending response. Please check your connection");
+        stream.write(response.as_bytes())?;
+        println!("Successfully responded");
     }else {
         //if any invaild request happens, send ERROR 404
-        let _ = stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
-        .expect("client has lost a connection or error while sending response. Please check your connection");
-        let _= stream.write(b"<h1>404</h1> <p>NOT FOUND</p>")
-        .expect("client has lost a connection or error while sending response. Please check your connection");
+        stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n <h1>404</h1> <p>NOT FOUND</p>")?;
+        println!("404 code sent");
     }
 
-}
-
-fn handle_client(stream: TcpStream){
-    //some other things might might be here
-    responed_as_file(stream);
+    Ok(())
 }
 
 fn main() -> std::io::Result<()>{
     let args: Vec<String> = env::args().collect();
-    let listener = TcpListener::bind(&args[2]).unwrap();
-
-    if args.len() > 2{
+    if args.len() <= 2{
         println!("Usage: ./<exe> <your_file_to_send> <Your IP:Port>");
+        exit(0);
     }
 
+    let path = &args[1];
+    let listener = TcpListener::bind(&args[2])?;
+
     for stream in listener.incoming(){
-        let stream = stream.unwrap();
+        let responder = responed_as_file(stream.expect("ERRPR"), path.clone());
 
         thread::spawn(move || {
-          handle_client(stream);
-        });
+          responder.expect("ERROR");
+        }).join().expect("Cannot join");
     }
 
     Ok(())
-
 }
